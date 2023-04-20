6cbbcf57519dcc31a1b6c23f8afb6d82c8aa13ce
Kacper <kmotyka@student.agh.edu.pl>
2023-04-20 12:41:16 +0200

    heuristic repaired (right accuracy now)

--- heuristics.py
+++ heuristics.py
@@ -12,13 +12,18 @@ def create_heuristic(X_train, y_train):
     return avg_cover_type
 
 def predict(test_data, heuristic):
+    
+    # Get cover type labels
+    mapping = heuristic.index.values
+    label = np.vectorize(lambda x: mapping[x])
+    
     # Calculate the difference between the test data and the heuristic
-    # print(test_data.to_numpy())
     diff_df = np.abs(heuristic.values - test_data.to_numpy()[:, None, :])
+
     # Sum the differences for each cover type
     ranking_df = diff_df.sum(axis=2)
 
     # Get the cover type with the lowest difference
     preds = np.argmin(ranking_df, axis=1)
-    
-    return preds.tolist()
+
+    return label(preds)
